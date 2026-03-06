db602c80a1fe231a7238f18a09a9b10c6bd86b5a
onkarkarande77 <karandeonkar05@gmail.com>
2026-03-06 19:16:11 +0530

    again practice of hibernate mapping

---
+++ Spring_MVC/spring_hib_practice/src/main/java/com/dao/EmployeeEntity_I.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import java.util.List;
+
+import com.bean.EmployeeBean;
+
+public interface EmployeeEntity_I {
+	boolean create(EmployeeBean eb);
+	List<EmployeeBean> getAllRecs();
+	boolean deleteRecs(int empId);
+	boolean userAuth(int empId,String empName);
+}
